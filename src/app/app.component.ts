3a6f15f54257a6445a0ef697925c770a2949ecd9
mert keskin <mertkeskinmtk6@gmail.com>
2023-12-10 22:49:10 +0300

    Remove unneccesary files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, MatFormFieldModule, MatInputModule],
+  imports: [CommonModule, RouterOutlet],
 
 })
 export class AppComponent {
